6441c6b1c550ba11966ef9ca63b018c1f2b843aa
Arnaud Marchand <amarchand@icloud.com>
2025-03-05 12:23:22 +0100

    v6.1.5b

--- server/main.py
+++ server/main.py
@@ -10,6 +10,7 @@ PREFIX = os.getenv("AMQC_PREFIX", "")
 
 app = FastAPI()
 
+
 app.mount("/AMQC", StaticFiles(directory="/opt/static"), name="static")
 
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
