1b2731e0dcca7da0e095ed39493f6ef118c92cf9
debockp <pierre.debock@equans.com>
2025-04-17 09:10:41 +0200

    v6.1.6

--- server/main.py
+++ server/main.py
@@ -15,51 +15,63 @@ app.mount("/AMQC", StaticFiles(directory="/opt/static"), name="static")
 
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
 async def req1(request:Request ):
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
     response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost"
-                                , headers=request.headers, data={})    
-    print("====>"*30)
-    print("http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
-    print(response.text)
-    print(request.headers)
+                                , headers=headers, data={})    
+    #print("====>"*30)
+    #print("http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
+    #print(response.text)
+    #print(request.headers)
     return response.json()
 
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=*")
 async def req2(request:Request ):
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
     response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=*"
-                                , headers=request.headers, data={})    
+                                , headers=headers, data={})    
     return response.json()
 
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,connector=clientConnectors,connectorName=*,connectionViewType=clientId,connectionName=*")
 async def req3(request:Request ):
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
     response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,connector=clientConnectors,connectorName=*,connectionViewType=clientId,connectionName=*"
-                                , headers=request.headers, data={})    
+                                , headers=headers, data={})    
     return response.json()
 
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName=*")
 async def req4(request:Request ):
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
     response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName=*"
-                                , headers=request.headers, data={})    
+                                , headers=headers, data={})    
     return response.json()
 
 @app.post(f"{PREFIX}/api/jolokia")
 async def jolpost(request:Request ):
-    print("====>"*30)
+    #print("====>"*30)
     body=await request.json()
-    print(body)
-    response = requests.request("POST", "http://localhost:8161/api/jolokia", headers=request.headers, data=json.dumps(body))
+    #print(body)
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
+    response = requests.request("POST", "http://localhost:8161/api/jolokia", headers=headers, data=json.dumps(body))
     return response.json()
 
 @app.post(f"{PREFIX}/api/message/{{target}}")
 async def jolmessage(request:Request,target,type,Origin,ID ):
-    print("====>"*30)
+    #print("====>"*30)
     body=await request.body()
-    print(body)
-    print(type)
+    #print(body)
+    #print(type)
     url=f"http://localhost:8161/api/message/{target}?type={type}&Origin={Origin}&ID={ID}"
-    print(url)
-    print(request.headers)
+    #print(url)
+    #print(request.headers)
+    auth = request.headers.get("authorization")
+    headers = {"authorization": auth, "referer": "http://localhost"}
     response = requests.request("POST", url
-                                , headers=request.headers, data=body.decode("utf-8"))
+                                , headers=headers, data=body.decode("utf-8"))
     
     print(response.text)
     return {"result":response.text}
