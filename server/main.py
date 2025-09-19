ea50a6d37a71228694e8f96cc406bbb1a16aa1fa
Arnaud Marchand <amarchand@icloud.com>
2025-09-19 17:12:54 +0200

    Use session in request in order to speed up rest calls on windows

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, Request
 
@@ -13,66 +14,61 @@ app = FastAPI()
 
 app.mount(f"{PREFIX}/AMQC", StaticFiles(directory="/opt/static"), name="static")
 
+session=None
+
+def get_session(req:Request):
+    global session
+    
+    if session is not None:        
+        return session
+
+    print("Create session")
+    session = requests.Session()
+    auth = req.headers.get("authorization")
+    if auth:
+        session.headers.update({"authorization": auth, "referer": "http://localhost"})
+    return session
+
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
 async def req1(request:Request ):
-    auth = request.headers.get("authorization")
-    headers = {"authorization": auth, "referer": "http://localhost"}
-    response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost"
-                                , headers=headers, data={})    
-    #print("====>"*30)
-    #print("http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
-    #print(response.text)
-    #print(request.headers)
+    print(">>>> 1")
+    response = get_session(request).get("http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost")
+    print("<<<< 1")
     return response.json()
 
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=*")
 async def req2(request:Request ):
-    auth = request.headers.get("authorization")
-    headers = {"authorization": auth, "referer": "http://localhost"}
-    response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=*"
-                                , headers=headers, data={})    
+    response = get_session(request).get("http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=*",)
     return response.json()
 
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,connector=clientConnectors,connectorName=*,connectionViewType=clientId,connectionName=*")
 async def req3(request:Request ):
-    auth = request.headers.get("authorization")
-    headers = {"authorization": auth, "referer": "http://localhost"}
-    response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,connector=clientConnectors,connectorName=*,connectionViewType=clientId,connectionName=*"
-                                , headers=headers, data={})    
+    #auth = request.headers.get("authorization")
+    #headers = {"authorization": auth, "referer": "http://localhost"}
+    response = get_session(request).get("http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,connector=clientConnectors,connectorName=*,connectionViewType=clientId,connectionName=*")
     return response.json()
 
 @app.get(f"{PREFIX}/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName=*")
 async def req4(request:Request ):
-    auth = request.headers.get("authorization")
-    headers = {"authorization": auth, "referer": "http://localhost"}
-    response = requests.request("GET", "http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName=*"
-                                , headers=headers, data={})    
+    #auth = request.headers.get("authorization")
+    #headers = {"authorization": auth, "referer": "http://localhost"}
+    response = get_session(request).get("http://localhost:8161/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName=*")
     return response.json()
 
 @app.post(f"{PREFIX}/api/jolokia")
 async def jolpost(request:Request ):
-    #print("====>"*30)
-    body=await request.json()
-    #print(body)
+    body = await request.json()
     auth = request.headers.get("authorization")
     headers = {"authorization": auth, "referer": "http://localhost"}
-    response = requests.request("POST", "http://localhost:8161/api/jolokia", headers=headers, data=json.dumps(body))
+    response = session.post("http://localhost:8161/api/jolokia", headers=headers, data=json.dumps(body))
     return response.json()
 
 @app.post(f"{PREFIX}/api/message/{{target}}")
-async def jolmessage(request:Request,target,type,Origin,ID ):
-    #print("====>"*30)
-    body=await request.body()
-    #print(body)
-    #print(type)
-    url=f"http://localhost:8161/api/message/{target}?type={type}&Origin={Origin}&ID={ID}"
-    #print(url)
-    #print(request.headers)
+async def jolmessage(request:Request, target, type, Origin, ID ):
+    body = await request.body()
+    url = f"http://localhost:8161/api/message/{target}?type={type}&Origin={Origin}&ID={ID}"
     auth = request.headers.get("authorization")
     headers = {"authorization": auth, "referer": "http://localhost"}
-    response = requests.request("POST", url
-                                , headers=headers, data=body.decode("utf-8"))
-    
+    response = session.post(url, headers=headers, data=body.decode("utf-8"))
     print(response.text)
-    return {"result":response.text}
-
+    return {"result": response.text}
